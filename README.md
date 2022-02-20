bdd48d259e683b5de117ad2be15dc27344e29a03
Flip Actual <flipactual@gmail.com>
2022-02-19 18:48:01 -0600

    add color test

---
+++ README.md
@@ -0,0 +1,9 @@
+# [Ray Tracing in One Weekend](https://raytracing.github.io/books/RayTracingInOneWeekend.html) in [Deno](https://deno.land/)
+
+Because I'm really funny
+
+## Color Test
+
+```sh
+deno run src/colorTest.ts > image.ppm
+```
---
+++ src/colorTest.ts
@@ -0,0 +1,25 @@
+const tE = new TextEncoder();
+
+const stdout = (x: string) => Deno.stdout.write(tE.encode(x));
+const stderr = (x: string) => Deno.stderr.write(tE.encode(x));
+
+const imageWidth = 255;
+const imageHeight = 255;
+
+await stdout(`P3
+${imageWidth} ${imageHeight}
+255
+`);
+
+for (let y = imageHeight - 1; y >= 0; y -= 1) {
+  await stderr(`\r${y} lines remaining`);
+  for (let x = 0; x < imageWidth; x += 1) {
+    const r = x / (imageWidth - 1);
+    const g = y / (imageHeight - 1);
+    const b = 0.25;
+    await stdout(
+      `${Math.floor(255 * r)} ${Math.floor(255 * g)} ${Math.floor(255 * b)}
+`,
+    );
+  }
+}
